65723078b5da507bfb559f0716b018fbb16b406a
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-05-06 10:30:25 +0900

    add post read finished

--- yomilog/app/views.py
+++ yomilog/app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from app.models import ReadHistory
+
+
+@csrf_exempt
+def insert_log(request):
+    d = json.loads(request.body)
+    ReadHistory.objects.create(
+        name=d["name"],
+        category=d["category"],
+        title=d["title"],
+        price=d["price"],
+        read_at=d["readAt"],
+        is_public=d["isPublic"],
+        is_favorite=d["isFavorite"],
+    )
+    return JsonResponse({})
